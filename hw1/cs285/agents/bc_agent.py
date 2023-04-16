5058651406a966fde38e60c08f02187ea1f3b28a
Pume Tuchinda <pumetuchinda@gmail.com>
2023-04-16 04:12:54 -0400

    add base code for hw1

--- hw1/cs285/agents/bc_agent.py
+++ hw1/cs285/agents/bc_agent.py
@@ -1,10 +1,12 @@
-from .base_agent import BaseAgent
 from cs285.infrastructure.replay_buffer import ReplayBuffer
-from cs285.policies.MLP_Policy import MLPPolicySL
+from cs285.policies.MLP_policy import MLPPolicySL
+from .base_agent import BaseAgent
+
 
 class BCAgent(BaseAgent):
     def __init__(self, env, agent_params):
-        super().__init__()
+        super(BCAgent, self).__init__()
+
         # init vars
         self.env = env
         self.agent_params = agent_params
@@ -16,20 +18,23 @@ class BCAgent(BaseAgent):
             self.agent_params['n_layers'],
             self.agent_params['size'],
             discrete=self.agent_params['discrete'],
-            learning_rate=self.agent_params['learning_rate']
+            learning_rate=self.agent_params['learning_rate'],
         )
+
+        # replay buffer
         self.replay_buffer = ReplayBuffer(self.agent_params['max_replay_buffer_size'])
-    
+
     def train(self, ob_no, ac_na, re_n, next_ob_no, terminal_n):
-        # training a BC refers to updating its actor using the given observations and corresponding action labels
-        log = self.actor.update(ob_no, ac_na)
+        # training a BC agent refers to updating its actor using
+        # the given observations and corresponding action labels
+        log = self.actor.update(ob_no, ac_na)  # HW1: you will modify this
         return log
-    
+
     def add_to_replay_buffer(self, paths):
         self.replay_buffer.add_rollouts(paths)
 
     def sample(self, batch_size):
-        return self.replay_buffer.sample_random_data(batch_size) # HW1: modify this
-    
+        return self.replay_buffer.sample_random_data(batch_size)  # HW1: you will modify this
+
     def save(self, path):
         return self.actor.save(path)
--- hw1/cs285/policies/MLP_policy.py
+++ hw1/cs285/policies/MLP_policy.py
@@ -2,15 +2,19 @@ import abc
 import itertools
 from typing import Any
 from torch import nn
-from torch.nn import function as F
+from torch.nn import functional as F
 from torch import optim
+
 import numpy as np
 import torch
 from torch import distributions
+
 from cs285.infrastructure import pytorch_util as ptu
 from cs285.policies.base_policy import BasePolicy
 
+
 class MLPPolicy(BasePolicy, nn.Module, metaclass=abc.ABCMeta):
+
     def __init__(self,
                  ac_dim,
                  ob_dim,
@@ -20,8 +24,10 @@ class MLPPolicy(BasePolicy, nn.Module, metaclass=abc.ABCMeta):
                  learning_rate=1e-4,
                  training=True,
                  nn_baseline=False,
-                 **kwargs):
+                 **kwargs
+                 ):
         super().__init__(**kwargs)
+
         # init vars
         self.ac_dim = ac_dim
         self.ob_dim = ob_dim
@@ -36,53 +42,75 @@ class MLPPolicy(BasePolicy, nn.Module, metaclass=abc.ABCMeta):
             self.logits_na = ptu.build_mlp(
                 input_size=self.ob_dim,
                 output_size=self.ac_dim,
-                n_layer=self.n_layers,
-                size=self.size
+                n_layers=self.n_layers,
+                size=self.size,
             )
             self.logits_na.to(ptu.device)
             self.mean_net = None
             self.logstd = None
-            self.optimizer = optim.Adam(self.logits_na.parameters(), self.learning_rate)
+            self.optimizer = optim.Adam(self.logits_na.parameters(),
+                                        self.learning_rate)
         else:
             self.logits_na = None
             self.mean_net = ptu.build_mlp(
                 input_size=self.ob_dim,
                 output_size=self.ac_dim,
-                n_layers=self.n_layers,
-                size=self.size
+                n_layers=self.n_layers, size=self.size,
             )
             self.mean_net.to(ptu.device)
-            self.logstd = nn.Parameter(torch.zeros(self.ac_dim, dtype=torch.float32, device=ptu.device))
+            self.logstd = nn.Parameter(
+                torch.zeros(self.ac_dim, dtype=torch.float32, device=ptu.device)
+            )
             self.logstd.to(ptu.device)
-            self.optimizer = optim.Adam(itertools.chain([self.logstd], self.mean_net.parameters()), self.learning_rate)
+            self.optimizer = optim.Adam(
+                itertools.chain([self.logstd], self.mean_net.parameters()),
+                self.learning_rate
+            )
+
+    ##################################
 
     def save(self, filepath):
         torch.save(self.state_dict(), filepath)
-    
+
+    ##################################
+
     def get_action(self, obs: np.ndarray) -> np.ndarray:
         if len(obs.shape) > 1:
             observation = obs
         else:
             observation = obs[None]
-        
-        # TODO: return the action that the policy prescribes
-        raise NotImplementedError()
 
-    def update(self, observation, actions, **kwargs):
-        raise NotImplementedError()
-    
+        # TODO return the action that the policy prescribes
+        raise NotImplementedError
+
+    # update/train this policy
+    def update(self, observations, actions, **kwargs):
+        raise NotImplementedError
+
+    # This function defines the forward pass of the network.
+    # You can return anything you want, but you should be able to differentiate
+    # through it. For example, you can return a torch.FloatTensor. You can also
+    # return more flexible objects, such as a
+    # `torch.distributions.Distribution` object. It's up to you!
     def forward(self, observation: torch.FloatTensor) -> Any:
-        raise NotImplementedError()
-    
+        raise NotImplementedError
+
+
+#####################################################
+#####################################################
+
 class MLPPolicySL(MLPPolicy):
     def __init__(self, ac_dim, ob_dim, n_layers, size, **kwargs):
         super().__init__(ac_dim, ob_dim, n_layers, size, **kwargs)
         self.loss = nn.MSELoss()
 
-    def update(self, observations, actions, adv_n=None, acs_labels_na=None, qvals=None):
-        # TODO: update policy and return the loss
+    def update(
+            self, observations, actions,
+            adv_n=None, acs_labels_na=None, qvals=None
+    ):
+        # TODO: update the policy and return the loss
         loss = TODO
         return {
-            # can add other logging info
-            "Training Loss": ptu.to_numpy(loss),
+            # You can add extra logging information here, but keep this line
+            'Training Loss': ptu.to_numpy(loss),
         }
